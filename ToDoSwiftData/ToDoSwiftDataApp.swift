96eb72dddf41169317bfd29344c08cc6b3f9d0d4
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-01-07 23:58:29 +0300

    Initial Commit

---
+++ ToDoSwiftData/ToDoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftDataApp.swift
+//  ToDoSwiftData
+//
+//  Created by Melik Demiray on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
