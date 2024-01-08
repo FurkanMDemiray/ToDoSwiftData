a27cd7d4498a3af59a00bb5def292539d8c4ac53
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-01-08 15:29:39 +0300

    added save & dismiss button with swiftdata

--- ToDoSwiftData/Screen/AddToDoScreen.swift
+++ ToDoSwiftData/Screen/AddToDoScreen.swift
@@ -6,19 +6,53 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddToDoScreen: View {
+
+    @Environment(\.modelContext) var context
+    @Environment(\.dismiss) var dismiss
 
     @State var name: String = ""
     @State var priority: Int?
 
     var body: some View {
-        VStack {
-            Form {
-                TextField("Name", text: $name)
-                TextField("Priority", value: $priority, format: .number)
+        NavigationStack {
+            VStack {
+                Form {
+                    TextField("Name", text: $name)
+                    TextField("Priority", value: $priority, format: .number)
+                }
+            }.navigationTitle("Add ToDo")
+                .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }, label: {
+                            Text("Dismiss")
+                        })
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+
+                        guard let priority = priority else { return }
+                        let toDo = ToDo(name: name, priority: priority)
+
+                        context.insert(toDo)
+                        do {
+                            try context.save()
+                        } catch {
+                            print(error)
+                        }
+
+                        dismiss()
+
+                    }, label: {
+                            Text("Save")
+                        })
+                }
             }
-        }.padding()
+        }
     }
 }
 
